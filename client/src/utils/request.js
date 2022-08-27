afdba0d98645aca8067636bf8442d3304b1f5ce0
RawJrk <rodjer7777@gmail.com>
2022-08-27 17:56:26 +0300

    refactoring server request functions

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,26 +1,33 @@
 import fetch from 'isomorphic-fetch';
 
-export const getChats = async () => {
-  const response = await fetch('/chats');
-
+const request = async (url, options) => {
+  const response = await fetch(url, options);
   const data = await response.json();
+  return data;
+}
+
+export const getChats = async () => {
+  const data = await request('/chats')
+  return data;
+};
+
+export const getSearch = async queryStr => {
+  const data = await request(`/search?q=${queryStr}`);
   return data;
 };
 
 export const getMessages = async userId => {
-  const response = await fetch(`/messages/${userId}`);
-
-  const data = await response.json();
+  const data = await request(`/messages/${userId}`);
   return data;
 };
 
 export const postMessage = async newMessage => {
-  const response = await fetch('/messages', {
+  const options = {
     method: 'POST',
     headers: { 'content-Type': 'application/json' },
     body: JSON.stringify({ newMessage }),
-  });
+  };
 
-  const data = await response.json();
+  const data = request('/messages', options);
   return data;
 };
