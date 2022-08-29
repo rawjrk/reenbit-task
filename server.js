418ff3a25c25f25fa3a8a01765d8995791361f32
RawJrk <rodjer7777@gmail.com>
2022-08-29 13:32:49 +0300

    fixed port issue for prod

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const { getChats } = require('./controllers/chats');
 const { getSearch } = require('./controllers/search');
